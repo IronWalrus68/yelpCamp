1b6cf38d7d0bb48935f078ef59d4b4540f5680d8
IronWalrus68 <camsjobhunting@gmail.com>
2023-10-21 16:28:38 +0100

    Initial commit: Added campground content
    Project Structure:
    - Routes, middleware, and pages have been set up.
    - Entry point: `index.js`. Start the project using "node index.js".
    
    Directories and their Descriptions:
    - `models/`: Contains schemas for `Campground` and `Review`.
    - `seeds/`: Houses data about cities for the campsites. The `index.js` and `seedHelpers.js` generate seed content, randomly selecting campsite names and locations.
    - `utils/`: Contains utility features:
      - `catchAsync`: Wraps asynchronous functions with try-catch.
      - `ExpressError`: Handles Express-related errors.
    - `views/`: Contains website content including pages, layouts, and other rendering-related files.

---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const cities = require('./cities')
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/Campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non commodi excepturi ullam voluptates. A nisi aspernatur totam quas! Veritatis repellat tempora non tenetur. Quas aspernatur inventore sint ex, consectetur molestiae!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
